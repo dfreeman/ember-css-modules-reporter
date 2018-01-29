8b1da233e962502e80bed64728a0174ad9bb86ba
Dan Freeman <dfreeman@salsify.com>
2018-01-29 17:56:24 -0500

    Upgrade ember-cli-qunit

--- tests/unit/generated-test-module-test.js
+++ tests/unit/generated-test-module-test.js
@@ -4,6 +4,6 @@ module('Unit | generated lint tests');
 
 test('the lint test module is generated', function(assert) {
   // More detailed tests take place in node land; this is just ensuring the tests make it into the build
-  window.require('dummy/tests/css-warnings.lint-test');
+  window.require('dummy/tests/css-warnings-0.lint-test');
   assert.ok(true, 'the module was successfully imported');
 });
